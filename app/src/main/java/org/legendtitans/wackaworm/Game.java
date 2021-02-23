d2919ec7f60a22a721bd52822883171f3fdb086c
BuildTools <unconfigured@null.spigotmc.org>
2021-02-23 11:28:49 -0700

    Textview

--- app/src/main/java/org/legendtitans/wackaworm/Game.java
+++ app/src/main/java/org/legendtitans/wackaworm/Game.java
@@ -3,13 +3,25 @@ package org.legendtitans.wackaworm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 
 public class Game extends AppCompatActivity {
-    String[] names = {"fart", "love"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        textView = findViewById(R.id.timerview);
+
+        new CountDownTimer(50000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                long sec = millisUntilFinished / 1000) ;
+                textView.setText(sec+"");
+            }
+            public void onFinish() {
+                textView.setText("0");
+
+            }
+        }.start();
     }
 
 }
