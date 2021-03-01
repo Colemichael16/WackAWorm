b1eab5a91e3a5c55aef4f03ec4afef4ea6bffca6
Colemichael16 <colemichael999@gmail.com>
2021-03-01 11:30:24 -0700

    Added viewID

--- app/src/main/java/org/legendtitans/wackaworm/Game.java
+++ app/src/main/java/org/legendtitans/wackaworm/Game.java
@@ -1,20 +1,40 @@
 package org.legendtitans.wackaworm;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.widget.ImageView;
 import android.widget.TextView;
-
 public class Game extends AppCompatActivity {
     TextView textView;
+    ImageView imageView;
+    ImageView imageView2;
+    ImageView imageView3;
+    ImageView imageView4;
+    ImageView imageView5;
+    ImageView imageView6;
+    ImageView imageView7;
+    ImageView imageView8;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         textView = findViewById(R.id.timerview);
+        imageView = findViewById(R.id.imageView);
+        imageView2 = findViewById(R.id.imageView2);
+        imageView3 = findViewById(R.id.imageView3);
+        imageView4 = findViewById(R.id.imageView4);
+        imageView5 = findViewById(R.id.imageView5);
+        imageView6 = findViewById(R.id.imageView6);
+        imageView7 = findViewById(R.id.imageView7);
+        imageView8 = findViewById(R.id.imageView8);
 
+
+        ImageView[] Worms = {imageView, imageView2, imageView3, imageView4, imageView5, imageView6, imageView7,imageView8 };
         new CountDownTimer(50000, 1000) {
             public void onTick(long millisUntilFinished) {
                 long sec = millisUntilFinished / 1000;
@@ -22,9 +42,21 @@ public class Game extends AppCompatActivity {
             }
             public void onFinish() {
                 textView.setText("0");
+                System.exit(0);
 
             }
         }.start();
+
+        new CountDownTimer(3000, 3000) {
+            public void onTick(long millisUntilFinished) {
+                long sec = millisUntilFinished / 1000;
+                textView.setText(sec+"");
+            }
+            public void onFinish() {
+                textView.setText("0");
+                System.exit(0);
+
+            }
     }
 
 }
